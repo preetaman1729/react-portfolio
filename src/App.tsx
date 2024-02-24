babb0b7865025ea8633c223939bba90497878c34
preetaman1729 <preetaman.github@gmail.com>
2024-02-24 18:14:31 +0300

    added the contact page route to the main App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+// # File name: App.tsx
+// # Student’s Name: Amanpreet Kaur
+// # StudentID: 301308654
+// # Date: 23rd February 2024
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
+import Contact from './components/Contact';
 import Projects from './components/Projects';
 import Services from './components/Services';
 import React
@@ -12,7 +18,7 @@ const App: React.FC = () => {
       <Routes>
         <Route path='/' Component={Home}/>
         <Route path='/about' Component={About}/>
-        
+        <Route path='/contact' Component={Contact}/>
         <Route path='/project' Component={Projects}/>
         <Route path='/services' Component={Services}/>
       </Routes>
