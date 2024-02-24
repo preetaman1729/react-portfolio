bfd96c836583283e1a7fc06ce3b5e92c0a78116f
preetaman1729 <preetaman.github@gmail.com>
2024-02-24 18:03:56 +0300

    added the home route to the main App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
 
-const App = () => { 
+import React
+ from 'react';
+const App: React.FC = () => {
   
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' Component={Home}/>
+        </Routes>
+    </Router>
+  )
 }
 
 export default App
